b8d278b5b279d11f096e247b2854622cb0f5283b
hwipl <33433250+hwipl@users.noreply.github.com>
2021-12-28 11:27:23 +0100

    add "commands" command line argument
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -1,8 +1,29 @@
 package cmd
 
-import "github.com/hwipl/schedule-events/internal/server"
+import (
+	"flag"
+	"log"
+
+	"github.com/hwipl/schedule-events/internal/command"
+	"github.com/hwipl/schedule-events/internal/server"
+)
+
+// parseCommandLine parses the command line arguments
+func parseCommandLine() {
+	cmdFile := flag.String("commands", "", "read commands from `file`")
+	flag.Parse()
+
+	// parse commands file
+	if *cmdFile == "" {
+		log.Fatal("no commands file specified")
+	}
+	if err := command.CommandsFromJSON(*cmdFile); err != nil {
+		log.Fatal(err)
+	}
+}
 
 // Run is the main entry point
 func Run() {
+	parseCommandLine()
 	server.Run()
 }
